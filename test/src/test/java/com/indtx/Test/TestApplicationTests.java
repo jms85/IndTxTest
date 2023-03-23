c2177f988f03c755e08f7759ea27597fc8cd20a1
Jose Maria Sanchidrian <jose.maria.sanchidrian.martin@gmail.com>
2023-03-23 13:40:13 +0100

    Create project with spring initilizr

---
+++ test/src/test/java/com/indtx/Test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indtx.Test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
